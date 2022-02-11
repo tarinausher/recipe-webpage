83520b613f1893152aee62530471676209bafdd2
Tarina Usher <tarinausher@gmail.com>
2022-02-11 11:55:31 -0500

    Initializing Vue.js files

---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img id="logo" class="logo" alt="logo" src="./assets/logo.png">
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Roboto, Calibri, Trebuchet;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+}
+
+#logo {
+  position: fixed; 
+  top: 0;
+  left: 0;
+}
+
+</style>
