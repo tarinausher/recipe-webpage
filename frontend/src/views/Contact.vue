86b56d31fe42bce4995b6c854410554efbaac747
Tarina Usher <tarinausher@gmail.com>
2022-02-17 13:34:10 -0500

    Contact page format and CSS nearly set up; next step: setting up functionality.

--- frontend/src/views/Contact.vue
+++ frontend/src/views/Contact.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="container">
     <form action="/thank-you">
-    <!-- Will need to edit as needed -->
+    <!-- Will need to implement sending  -->
     <label for="fname">First Name</label>
     <input type="text" id="fname" name="firstname" placeholder="First name">
 
@@ -14,6 +14,7 @@
     <label for="subject">Subject</label>
     <textarea id="subject" name="subject" placeholder="Your comments here!" style="height:200px"></textarea>
 
+<!-- Will need to do a v-on:click take to thank -->
     <input type="submit" value="Submit">
 
   </form>
@@ -27,5 +28,40 @@ export default {
 </script>
 
 <style>
+form {
+    margin: 0 auto;
+}
 
+input, textarea {
+  width: 100%; 
+  padding: 12px; 
+  border: 1px solid #f6c453; 
+  border-radius: 4px; 
+  box-sizing: border-box; 
+  margin-top: 6px; 
+  margin-bottom: 16px; 
+  resize: vertical;
+}
+
+input[type=submit] {
+  background-color: #f6c453;
+  color: #fefbe9;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  box-shadow: inset 0 0 0 0 #f6c453;
+  transition: ease-out 0.3s;
+}
+
+input[type=submit]:hover {
+  box-shadow: inset 300px 0 0 0 #fefbe9;
+  background-color: #fefbe9;
+  color: #f6c453;
+}
+
+.container {
+  border-radius: 5px;
+  padding: 20px;
+}
 </style>
--- frontend/src/views/ThankYou.vue
+++ frontend/src/views/ThankYou.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Thank you for your comments!</h1>
+  <h1 class='thank-you'>Thank you for your comments!</h1>
 </template>
 
 <script>
@@ -9,5 +9,12 @@ export default {
 </script>
 
 <style>
-
+.thank-you {
+  font-family: "Meow Script";
+  font-size: 64px;
+  color: #183a1d;
+  letter-spacing: 1px;
+  text-align: center;
+  padding-top: 1.5em;
+}
 </style>
